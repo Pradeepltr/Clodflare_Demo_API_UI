457cdd97c3cb85328613f65bf216cbc2cd88fc22
Pradeep Kumar <pk6361439@gmail.com>
2022-10-10 13:14:41 +0530

    final commit

---
+++ UI/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import UserUI from './Component/UserUI';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to shop now </h1>
+      <UserUI />
+    </div>
+  );
+}
+
+export default App;
